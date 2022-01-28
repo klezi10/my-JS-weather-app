fea3f91537dc7faa66d76c11ac76c91948dfe159
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-01-28 10:20:19 +0700

    organized the variables at the top

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const cityInput = document.getElementById('city-input');
-
 // const apiKey = `2fc59c88fa863e451b6f4e4accf6d955`;
-
+const cityInput = document.getElementById('city-input');
 // const searchBtn = document.getElementById('search-btn');
 const form = document.getElementById('form');
 const cityName = document.getElementById('city-name');
