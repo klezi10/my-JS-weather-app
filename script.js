8889791245f949d627aea5bf8fca25018c1046e2
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-01-28 11:24:20 +0700

    set up default city

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // const apiKey = `2fc59c88fa863e451b6f4e4accf6d955`;
 const cityInput = document.getElementById('city-input');
+
+// console.log(city);
 // const searchBtn = document.getElementById('search-btn');
 const form = document.getElementById('form');
 const cityName = document.getElementById('city-name');
+
+//=================DEFAULT CITY=================
+defaultCity('Phuket');
+
+function defaultCity(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2fc59c88fa863e451b6f4e4accf6d955&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+//=====================DEFAULT CITY=====================
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -10,11 +22,11 @@ form.addEventListener('submit', (event) => {
 });
 
 function findCity() {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=2fc59c88fa863e451b6f4e4accf6d955&units=metric`;
-  axios.get(apiUrl).then(searchCity);
+  const city = cityInput.value;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2fc59c88fa863e451b6f4e4accf6d955&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
 }
 
-function searchCity(response) {
-  console.log(response);
+function displayWeather(response) {
   cityName.innerHTML = response.data.name;
 }
