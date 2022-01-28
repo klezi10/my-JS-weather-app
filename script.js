a60f2b340c68f994983a3837863d2f6ccb49fc71
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-01-28 10:19:31 +0700

    set up search city to show up in the DOM from the API

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-const apiKey = `2fc59c88fa863e451b6f4e4accf6d955`;
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Phuket&appid=2fc59c88fa863e451b6f4e4accf6d955`;
-
 const cityInput = document.getElementById('city-input');
+
+// const apiKey = `2fc59c88fa863e451b6f4e4accf6d955`;
+
 // const searchBtn = document.getElementById('search-btn');
 const form = document.getElementById('form');
 const cityName = document.getElementById('city-name');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  console.log(cityInput.value);
-  cityName.innerHTML = cityInput.value;
+  findCity();
 });
+
+function findCity() {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=2fc59c88fa863e451b6f4e4accf6d955&units=metric`;
+  axios.get(apiUrl).then(searchCity);
+}
+
+function searchCity(response) {
+  console.log(response);
+  cityName.innerHTML = response.data.name;
+}
