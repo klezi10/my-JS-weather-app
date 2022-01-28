9a3f23a2469cf630d9add87bae4cd4b46c061413
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-01-28 09:59:16 +0700

    set up input value to show city in the DOM

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const apiKey = `2fc59c88fa863e451b6f4e4accf6d955`;
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Phuket&appid=2fc59c88fa863e451b6f4e4accf6d955`;
 
+const cityInput = document.getElementById('city-input');
+// const searchBtn = document.getElementById('search-btn');
+const form = document.getElementById('form');
+const cityName = document.getElementById('city-name');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log(cityInput.value);
+  cityName.innerHTML = cityInput.value;
+});
